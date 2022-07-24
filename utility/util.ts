a93f1a95e19267d743953dfb08534e075073f9f0
David muia <9davidmuia@gmail.com>
2022-07-24 12:20:10 +0300

    first commit

---
+++ utility/util.ts
@@ -0,0 +1,17 @@
+import { orange } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const mainTheme = createTheme({
+    typography: {
+        fontFamily: " 'Finlandica', sans-serif"
+
+    },
+    palette: {
+        primary: {
+            main: "#424769",
+
+        },
+    },
+});
+
+export default mainTheme;
